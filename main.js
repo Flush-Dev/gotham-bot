3807962d97fa84c6f4614dc5552a50f60a542026
Flush dev <flushthedev@gmail.com>
2019-11-02 20:39:08 +0100

    Changements

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
-const Discord = require('discord.js');
+const { Client, MessageEmbed } = require('discord.js');
+const { TOKEN, PREFIX } = require('./config');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log("Je suis pret !");
+  client.user.setActivity('Gotham', { type: 'STREAMING' })
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  var args = msg.content.split(` `).slice(1).join(' ');
+  if (msg.content.startsWith(`${PREFIX}ping`)) msg.channel.send('Pong!');
+  if (msg.content == 'Lulu') msg.channel.send("Ma couille droite que je caresse chaque matin");
+  if (msg.content == 'Gotham') msg.channel.send("Le meilleur serveur !");
+  if (msg.content == 'Leey') msg.channel.send("La femme de Flush, a ne pas touchée !")
+  if (msg.content.startsWith(`${PREFIX}say`)) { 
+    msg.delete();
+     msg.channel.send(args);
   }
+     if (msg.content.startsWith(`${PREFIX}pp`)) {
+       msg.delete();
+       const pp = new MessageEmbed()
+       .setAuthor('Gotham')
+       .setThumbnail(`${message.author.avatarURL}`)
+       msg.channel.send(pp);
+     };
+  
 });
-
-client.login('NjM4OTE1NTExNjYwNTc2ODIx.Xby5Mw.ReM42aa3bnKO3tAPW-8Q4XXv0BE');
+client.login(TOKEN);
